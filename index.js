e6f0e25b9ec4f6c7fd3a8971fe3514dd300d19fc
gusavocarnovale <gustavocarnovale@gmail.com>
2021-08-08 23:22:08 -0300

    cambio en gral

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const uno = document.querySelector(".uno")
-const dos = document.querySelector(".dos")
-const titulo = document.querySelector(".titulo")
-const tituloDos = document.querySelector(".tituloDos")
-const tituloTres = document.querySelector(".tituloTres")
-
-
-
+const uno = document.querySelector(".uno");
+const dos = document.querySelector(".dos");
+const titulo = document.querySelector(".titulo");
+const tituloDos = document.querySelector(".tituloDos");
+const tituloTres = document.querySelector(".tituloTres");
 
 var options = {
   enableHighAccuracy: true,
@@ -21,43 +18,61 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 
 function success(position) {
   var coordenadas = position.coords;
-
   console.log("Tu posición actual es:");
   console.log("Latitud : " + coordenadas.latitude);
   console.log("Longitud: " + coordenadas.longitude);
   console.log("Más o menos " + coordenadas.accuracy + " metros.");
-     latitud = coordenadas.latitude;
-    longitud = coordenadas.longitude;
+  latitud = coordenadas.latitude;
+  longitud = coordenadas.longitude;
 
-    urlCl="https://api.openweathermap.org/data/2.5/weather?lat=" + latitud + "&lon=" + longitud + "&lang=es&appid=4452498fac43e15d8736c08eb4b37050&units=metric"
-    fetch(urlCl)
-      .then((response) => response.json())
-      .then((data) => {
-            console.log("Gracias por utlizar Climatico!")
-            console.log()           
-            console.log(data)
-            console.log(data.name)
-            console.log(data.main.pressure)
-            console.log(data.main.humidity)
-            console.log(data.weather[0].icon)
-            titulo.innerHTML +="<div class='col-sm-12 col-md-12 col-lg-12 col-xl-12 fuente'><p>Tu ubicacion actual: " + data.name + " - " + ciudad + "</p></div>";
-            tituloDos.innerHTML +="<div class= 'col-sm-12 col-md-12 col-lg-12 col-xl-12 fuente'><p >Temperatura actual: " + Math.round(data.main.temp) + " °C</p></div>";
-            tituloTres.innerHTML +="<div class='col col-sm-12 col-md-12 col-lg-12 col-xl-12 fuente'><img class='fondoIcono' src='http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png'/></div>";
-            tituloTres.innerHTML +="<div class='col col-sm-12 col-md-12 col-lg-12 col-xl-12 fuente'><p>" + data.weather[0].description+ " </p></div>";
-            uno.innerHTML +="<div class='col col-sm-3 col-md-3 col-lg-3 col-xl-3 fuente'><p>Maxima de: " + Math.round(data.main.temp_max)+ "°C</p></div>";
-            uno.innerHTML +="<div class='col col-sm-3 col-md-3 col-lg-3 col-xl-3 fuente'><p>Minima de: " + Math.round(data.main.temp_min)+ "°C</p></div>";
-            uno.innerHTML +="<div class='col col-sm-3 col-md-3 col-lg-3 col-xl-3 fuente'><p>Humedad: " + data.main.humidity + "%</p></div>";
-          });
+  urlCl =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    latitud +
+    "&lon=" +
+    longitud +
+    "&lang=es&appid=4452498fac43e15d8736c08eb4b37050&units=metric";
+  fetch(urlCl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Gracias por utlizar Climatico!");
+      titulo.innerHTML +=
+        "<div class='col-sm-12 col-md-12 col-lg-12 col-xl-12 fuente'><p>Tu ubicacion actual: " +
+        data.name +
+        " - " +
+        ciudad +
+        "</p></div>";
+      tituloTres.innerHTML +=
+        "<div class='col col-sm-12 col-md-12 col-lg-12 col-xl-12 fuente'><img class='fondoIcono' src='http://openweathermap.org/img/wn/" +
+        data.weather[0].icon +
+        ".png'/></div>";
+      tituloTres.innerHTML +=
+        "<div class='col col-sm-12 col-md-12 col-lg-12 col-xl-12 fuente'><p>" +
+        data.weather[0].description +
+        " </p></div>";
+      tituloDos.innerHTML +=
+        "<div class= 'col-sm-12 col-md-12 col-lg-12 col-xl-12 fuente'><p >Temperatura actual: " +
+        Math.round(data.main.temp) +
+        " °C</p></div>";
+      uno.innerHTML +=
+        "<div class='col col-sm-3 col-md-3 col-lg-3 col-xl-3 fuente'><p>Maxima de: " +
+        Math.round(data.main.temp_max) +
+        "°C</p></div>";
+      uno.innerHTML +=
+        "<div class='col col-sm-3 col-md-3 col-lg-3 col-xl-3 fuente'><p>Minima de: " +
+        Math.round(data.main.temp_min) +
+        "°C</p></div>";
+      uno.innerHTML +=
+        "<div class='col col-sm-3 col-md-3 col-lg-3 col-xl-3 fuente'><p>Humedad: " +
+        data.main.humidity +
+        "%</p></div>";
+    });
 
-    
   var platform = new H.service.Platform({
     apikey: "Pxv2IuJ72cpY9D4AgsobbOpYUZMYLJ0x1IcQvsy0mis",
   });
 
-  // Obtain the default map types from the platform object:
   var defaultLayers = platform.createDefaultLayers();
 
-  // Instantiate (and display) a map object:
   var map = new H.Map(
     document.querySelector("#mapContainer"),
     defaultLayers.vector.normal.map,
@@ -66,37 +81,21 @@ function success(position) {
       center: { lat: coordenadas.latitude, lng: coordenadas.longitude },
     }
   );
-
   (coords = { lat: coordenadas.latitude, lng: coordenadas.longitude }),
     (marker = new H.map.Marker(coords));
 
-  // Add the marker to the map and center the map at the location of the marker:
   map.addObject(marker);
   map.setCenter(coords);
-
-  // Get an instance of the search service:
   var service = platform.getSearchService();
 
-  // Call the reverse geocode method with the geocoding parameters,
-  // the callback and an error callback function (called if a
-  // communication error occurs):
   service.reverseGeocode(
     {
       at: coordenadas.latitude + "," + coordenadas.longitude,
-    }, 
+    },
     (result) => {
       result.items.forEach((item) => {
-        // Assumption: ui is instantiated
-        // Create an InfoBubble at the returned location with
-        // the address as its contents:
-      //  ui.addBubble(
-      //    new H.ui.InfoBubble(item.position, {
-      //      content: item.address.label,
-      //    })
-      //  );
-
-        ciudad = item.address.state
-      console.log(ciudad)
+        ciudad = item.address.state;
+        console.log(ciudad);
       });
     },
     alert
